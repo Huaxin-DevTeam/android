a2324abd41caa0a360c2920e3b0cc0a54243593e
Fidel Montesino <fidel.montesino@mobetia.com>
2014-06-16 15:59:46 +0200

    Prod

--- Huaxin/src/es/mihx/app/service/API.java
+++ Huaxin/src/es/mihx/app/service/API.java
@@ -2,7 +2,7 @@ package es.mihx.app.service;
 
 public class API {
 
-	public static final String HOST = "http://dev.mihx.es/api";
+	public static final String HOST = "http://www.mihx.es/api";
 
 	public static final String INIT = HOST + "/init";
 	public static final String SEARCH = HOST + "/search";
